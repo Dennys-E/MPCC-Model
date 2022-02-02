9b7643f8b3a1116d6d08ad08fe3159506df93437
Amber <ambertewinkel@gmail.com>
2022-02-02 15:41:36 +0100

    Fixed solar, cloudless sky

--- MPCCmodel.c
+++ MPCCmodel.c
@@ -35,7 +35,7 @@ gcc -Wall -o MPCCmodel.exe MPCCmodel.c ascii.o repwvl_thermal.o repwvl_solar.c -
 // prototypes
 int integrate_radiation(int nlev, double *T, double dp, double dt, double **tau_thermal, double *wvl_thermal, double *weight_thermal, int nwvl_thermal, double **tau_solar, double *wvl_solar, double *weight_solar, double *E0_solar, int nwvl_solar);
 int thermal_radiative_transfer(double *T, double *Edownthermal, double *Eupthermal, int nlev, int nwvl_thermal, double *wvl_thermal, double *weight_thermal, double **tau_thermal);
-int schwarzschild(double *T, double *B, double *tau_array, double *Eup, double *Edown, int nlev);
+int schwarzschild(double *B, double *tau_array, double *Eup, double *Edown, int nlev);
 int solar_radiative_transfer(double dp, double *Edownsolar, double *Eupsolar, int nlev, int nwvl_solar, double *wvl_solar, double *weight_solar, double *E0_solar, double **tau_solar);
 int irradiancetoT(double *Edowntot, double *Euptot, double *T, double dp, double dt);
 int convection(double *T, double *p);
@@ -52,7 +52,7 @@ double **memalloc2D(double **matrix, int nrows, int ncols);
 void eddington_v2 (double dtau, double g, double omega0, double mu0, double *t, double *r, double *rdir, double *sdir, double *tdir);
 
 int main()
-{
+{    
     // First step in determining runtime of the program
     clock_t start, end;
     double cpu_time_used;
@@ -103,7 +103,7 @@ int main()
     
     // Allocate heap memory
     plevPa   = calloc (nlev, sizeof(double));
-    O2_VMR   = calloc (nlev, sizeof(double)); // Note: using initialize_double_array gives a 'Segmentation fault' error
+    O2_VMR   = calloc (nlev, sizeof(double));
     N2_VMR   = calloc (nlev, sizeof(double));
     HNO3_VMR = calloc (nlev, sizeof(double));
     CO_VMR   = calloc (nlev, sizeof(double));
@@ -177,9 +177,7 @@ int main()
         read_tau("./ReducedLookupFile_thermal_50wvls_corrected.nc", nlev, plevPa, Tlev, H2O_VMR, CO2_VMR, O3_VMR, N2O_VMR, CO_VMR, CH4_VMR, O2_VMR, HNO3_VMR, N2_VMR, &tau_thermal, &wvl_thermal, &weight_thermal, &nwvl_thermal, 1); // defined at levels, choose 50 or 100     
         if (t_temp>=60*60*5*24)
         {
-            read_tau_solar("./ReducedLookupFile_solar_50wvls.nc", nlev, plevPa, Tlay,
-              H2O_VMR_lay, CO2_VMR_lay, O3_VMR_lay, N2O_VMR_lay, CO_VMR_lay, CH4_VMR_lay, O2_VMR_lay, HNO3_VMR_lay, N2_VMR_lay,
-              &tau_solar, &wvl_solar, &weight_solar, &E0_solar, &nwvl_solar, 0); // defined at layers
+            read_tau_solar("./ReducedLookupFile_solar_50wvls.nc", nlev, plevPa, Tlay, H2O_VMR_lay, CO2_VMR_lay, O3_VMR_lay, N2O_VMR_lay, CO_VMR_lay, CH4_VMR_lay, O2_VMR_lay, HNO3_VMR_lay, N2_VMR_lay, &tau_solar, &wvl_solar, &weight_solar, &E0_solar, &nwvl_solar, 0); // defined at layers
             t_temp = 0;
         }
         
@@ -218,25 +216,21 @@ int main()
         fprintf(fptr, "%9.0f \t \t %7.3f \n", t / 3600, Tlay[NLAY - 1]);
 
         // Condition to end run early (needs readjustment I guess)
-        /*
-        if (absdT_max < 0.01)
+        if (absdT_max < 0.0001)
         {
             printf("Ran into break.\n");
             break;
         }
-        */
 
         // Print progress in current timestep
         progress = 100. * (t / SIMTIME);
-        /*
         printf("Running the time loop... %3.2f  |  dt = %3.2f seconds  |  t = %3.2f days  |  Tsurface = %3.2f K \r", progress, dt, t / 86400, Tlay[NLAY - 1]);
-        fflush(stdout); */
+        fflush(stdout);
     }
     fclose(fptr);
 
     // Write file with the final layer-temperature structure
-    FILE *fptr2;
-    fptr2 = fopen("output_Tstructure_MPCCmodel.dat", "w");
+    FILE *fptr2 = fopen("output_Tstructure_MPCCmodel.dat", "w");
     fprintf(fptr2, "Final time = %5.0f hours, T (R) in different layers (L):\n", t / 3600);
     for (int i = 0; i < NLAY; i++)
     {
@@ -310,7 +304,7 @@ int thermal_radiative_transfer(double *T, double *Edownthermal, double *Euptherm
         }
 
         // Compute irradiance from the temperature profile
-        schwarzschild(T, B, tau_array, Eup, Edown, nlev);
+        schwarzschild(B, tau_array, Eup, Edown, nlev);
 
         // Integrate over different wavelength contributions
         for (int i = 0; i < nlev; i++)
@@ -322,7 +316,7 @@ int thermal_radiative_transfer(double *T, double *Edownthermal, double *Euptherm
     return 0;
 }
 
-int schwarzschild(double *T, double *B, double *tau_array, double *Eup, double *Edown, int nlev)
+int schwarzschild(double *B, double *tau_array, double *Eup, double *Edown, int nlev)
 {
     // Function computing irradiance from radiance, independent of wavelengths
     // I: T array, B array, tau_array array
@@ -398,8 +392,6 @@ int irradiancetoT(double *Edowntot, double *Euptot, double *T, double dp, double
 
 int solar_radiative_transfer(double dp, double *Edownsolar, double *Eupsolar, int nlev, int nwvl_solar, double *wvl_solar, double *weight_solar, double *E0_solar, double **tau_solar)
 {
-    double E0_temp = 0.;
-    
     // Clear the relevant arrays before each use of the function
     double Edir[nlev], Edir_wvl[nlev];
     for(int i = 0; i < nlev; i++){
@@ -409,89 +401,80 @@ int solar_radiative_transfer(double dp, double *Edownsolar, double *Eupsolar, in
         Edir_wvl[i] =0.;
      }
     
-    // Define quantities and tau_Rayleigh[i][w] (even though it does not depend on i --> better compatibility with tau)
+    // Define quantities and tau_Rayleigh[w][i] (even though it does not depend on i --> better compatibility with tau)
     double tau_Rayleigh[nwvl_solar][NLAY];
     double sigma_to_tau = dp*100/(M_AIR*U*G);
     
     for(int i = 0; i < NLAY; i++){
         for(int w = 0; w < nwvl_solar; w++){
-            tau_Rayleigh[w][i] = sigma_to_tau*(4.840E-31 * (550.0/ 
-            wvl_solar[w])*(550.0/wvl_solar[w])*(550.0/wvl_solar[w])*(550.0/wvl_solar[w]));
-            //printf(%8.2f %8.2f, wvl_solar[w], tau_Rayleigh)
+            tau_Rayleigh[w][i] = sigma_to_tau*(4.840E-31 * (550.0/wvl_solar[w])*(550.0/wvl_solar[w])*(550.0/wvl_solar[w])*(550.0/wvl_solar[w]));
         }
     }
 
-    // This is the point to later define omega0 and tau for clouds. For now I leave them set to zero
+    // This is the point to later define omega0 and tau for clouds. For now we leave them set to zero
     double tau_cloud[nwvl_solar][NLAY];
     double tau_tot[nwvl_solar][NLAY];
     double omega0[nwvl_solar][NLAY];
-    
+        
     for(int i = 0; i < NLAY; i++){
         for(int w = 0; w < nwvl_solar; w++){
-            tau_tot[w][i] = tau_Rayleigh[w][i]+tau_cloud[w][i]+tau_solar[w][i];
-            omega0[w][i] = tau_Rayleigh[w][i]/tau_solar[w][i]; //here a cloud scattering tau would have to be added in the enumerator
+            tau_cloud[w][i] = 0;
+            tau_tot[w][i] = tau_Rayleigh[w][i]+tau_cloud[w][i]+tau_solar[w][i]; // tau_ext in lecture notes
+            if(tau_tot[w][i] > 100){
+                tau_tot[w][i] = 100.;
+            }
+            omega0[w][i] = tau_Rayleigh[w][i]/tau_tot[w][i]; //here a cloud scattering tau would have to be added in the enumerator
         }
     }
     
     // In order to compute the eddington function for the adding routine, all necessary layer quantites have to be defined
     // Since no clouds are introduced yet, g is set to zero.
     double t[NLAY], r[NLAY], rdir[NLAY], sdir[NLAY], tdir[NLAY];
-    double R[NLAY], T[NLAY], S[NLAY], Sdir[NLAY], Tdir[NLAY];
+    double R[NLAY], T[NLAY], Sdir[NLAY], Tdir[NLAY];
     
-    double g = G_RAYLEIGH; //asymetry parameter
+    double g = G_RAYLEIGH; //asymmetry parameter
     double mu0 = 0.5; // 60 degree solar zenith angle
 
     for(int w = 0; w < nwvl_solar; w++){
         for(int i = 0; i < NLAY; i++){
-            // This loop over wavelengths will be relevant for a wile. E.g. all quantites with the suffix _wvl are relevant for one wavelength and will be added in the end of the loop.
+            // This loop over wavelengths will be relevant for a while. E.g. all quantites with the suffix _wvl are relevant for one wavelength and will be added in the end of the loop.
             eddington_v2 (tau_tot[w][i], g, omega0[w][i], mu0, &t[i],
                           &r[i], &rdir[i], &sdir[i], &tdir[i]);
         }
         
         // Define boundary conditions and "initial values"
-        Edir_wvl[0] = E0_solar[w]*mu0/2.;
-        E0_temp += E0_solar[w]*weight_solar[w];
-        //printf("E0[w]: %8.2f \n", E0_solar[w]);
-        R[0] = r[0];
-        T[0] = t[0];
-        Tdir[0] = tdir[0];
-        Sdir[0] = sdir[0];
-        Sdir[1] = (t[1] * sdir[0] + tdir[0] * rdir[1] * r[0] * t[1])/(1 - r[0]*r[1]) + tdir[0] * sdir[1];
+        Edir_wvl[0] = E0_solar[w]*mu0/2.; 
+        R[0] = r[0]; 
+        T[0] = t[0];  
+        Tdir[0] = tdir[0]; 
+        Sdir[0] = sdir[0]; 
         
         // Compute R, T and Tdir
         for (int i = 0; i < NLAY-1; i++)
         {
-            R[i+1] = r[i+1] + R[i]*t[i+1]*t[i+1]/(1-R[i]*r[i+1]);
-            T[i+1] = T[i]*t[i+1]/(1-R[i]*r[i+1]);
-            Tdir[i+1] = Tdir[i] * tdir[i+1];
+            R[i+1] = r[i+1] + R[i]*t[i+1]*t[i+1]/(1-R[i]*r[i+1]); 
+            T[i+1] = T[i]*t[i+1]/(1-R[i]*r[i+1]); 
+            Tdir[i+1] = Tdir[i] * tdir[i+1]; 
         }
         
         // Using T, compute direct downward irradiance 
-        for (int i = 1; i<nlev; i++){
-        Edir_wvl[i] = Tdir[i-1] * Edir_wvl[0];
+        for (int i = 1; i < nlev; i++){
+            Edir_wvl[i] = Tdir[i-1] * Edir_wvl[0];
         }
         
         // Now the remaining components of Sdir can be computed
-        for (int i =0; i < NLAY-1; i++){
-            Sdir[i+1] = (t[i+1]*Sdir[i]+Tdir[i]*rdir[i+1]*R[i]*t[i+1])/(1-R[i]*r[i+1])+Tdir[i]*sdir[i+1];
+        for (int i = 0; i < NLAY-1; i++){
+            Sdir[i+1] = (t[i+1]*Sdir[i]+Tdir[i]*rdir[i+1]*R[i]*t[i+1])/(1-R[i]*r[i+1])+Tdir[i]*sdir[i+1]; 
         }
         
         // Define surface albedo Ag and boundary conditions for Eupsolar and Edownsolar in current wavelength
         double Ag = 0.12;
         double Edownsolar_wvl[nlev], Eupsolar_wvl[nlev];
         
-        // Clear _wvl arrays 
-        /*
-        for (int i=0; i<NLAY-1; i++){
-            Edownsolar_wvl[i]=0.;
-            Eupsolar_wvl[i]=0.;
-        }
-        */
-        
         Edownsolar_wvl[0] = 0.;
-        Edownsolar_wvl[NLAY] = Edir_wvl[0]*(Sdir[NLAY-1]+Tdir[NLAY-1]*R[NLAY-1]*Ag)                                                      /(1-R[NLAY-1]*Ag);
-        Eupsolar_wvl[NLAY] = Ag*(Edownsolar_wvl[NLAY]+Tdir[NLAY-1]*Edir_wvl[0]);
-        
+        Edownsolar_wvl[NLAY] = Edir_wvl[0]*(Sdir[NLAY-1]+Tdir[NLAY-1]*R[NLAY-1]*Ag)/(1-R[NLAY-1]*Ag); 
+        Eupsolar_wvl[NLAY] = Ag*(Edownsolar_wvl[NLAY]+Tdir[NLAY-1]*Edir_wvl[0]); 
+
         // Calculate the remaining components of Eupsolar and Edownsolar in current wavelength, going from bottom to TOA
         for (int i = NLAY; i>1; i--){
             Edownsolar_wvl[i-1]=(R[i-2]*t[i-1]*Eupsolar_wvl[i]+Edir_wvl[0]*Sdir[i-2]+Edir_wvl[i-1]*rdir[i-1]*R[i-2])/(1-R[i-2]*r[i-1]);
@@ -500,7 +483,7 @@ int solar_radiative_transfer(double dp, double *Edownsolar, double *Eupsolar, in
         Eupsolar_wvl[0] = t[0]*Eupsolar_wvl[1]; //technically + r[0]*Edownsolar_wvl[0] but second term is set to zero
         
         // Adding up _wvl contributions to the returned irradiances
-        for (int i=0; i<nlev; i++){
+        for (int i = 0; i < nlev; i++){
             Edir[i] += Edir_wvl[i]*weight_solar[w];
             Eupsolar[i] += Eupsolar_wvl[i]*weight_solar[w];
             Edownsolar[i] += (Edownsolar_wvl[i])*weight_solar[w];
@@ -508,15 +491,9 @@ int solar_radiative_transfer(double dp, double *Edownsolar, double *Eupsolar, in
     } // end of wavelength loop
     
     for (int i=0; i<nlev; i++){
-    Edownsolar[i] = Edownsolar[i] + Edir[i];
-        }
-    //printf("%8.2f", E0_temp);
-    /*
-        printf("Eupsolar\n");
-        show(Eupsolar);
-        printf("Edownsolar\n");
-        show(Edownsolar);
-        printf("Edir\n"); */
+        printf("Level %d: Edir %6.3f \t Edownsolar %6.3f \t Eupsolar %6.3f\n", i, Edir[i], Edownsolar[i], Eupsolar[i]);
+       // Edownsolar[i] = Edownsolar[i] + Edir[i];
+    }
     exit(0);
     return 0;
 }
@@ -590,7 +567,7 @@ int show(const void *v)
 void initialize_double_array(double *array, size_t length, double value)
 {
     // Function to initialize an array to a specified constant
-    for (int i = 0; i < length; i++)
+    for (size_t i = 0; i < length; i++)
     {
         array[i] = value;
     }
@@ -647,47 +624,49 @@ double **memalloc2D(double **matrix, int nrows, int ncols)
     return matrix;
 }
 
-void eddington_v2 (double dtau, double g, double omega0, double mu0, double *t, double *r, double *rdir, double *sdir, double *tdir)
+
+/* calculate layer properties t, r, rdir, sdir, and tdir from            */
+/* layer optical thickness dtau, asymmetry parameter g,                  */
+/* single scattering albedo omega0, and cosine of solar zenith angle mu0 */
+
+void eddington_v2 (double dtau, double g, double omega0, double mu0,
+		   double *t, double *r, double *rdir, double *sdir, double *tdir)
 {
-    /* calculate layer properties t, r, rdir, sdir, and tdir from            */
-    /* layer optical thickness dtau, asymmetry parameter g,                  */
-    /* single scattering albedo omega0, and cosine of solar zenith angle mu0 */  
-    
-    double alpha1=0, alpha2=0, alpha3=0, alpha4=0, alpha5=0, alpha6=0;
-    double a11=0, a12=0, a13=0, a23=0, a33=0;
-    double lambda=0, b=0, A=0;
-    double denom=0;
+  double alpha1=0, alpha2=0, alpha3=0, alpha4=0, alpha5=0, alpha6=0;
+  double a11=0, a12=0, a13=0, a23=0, a33=0;
+  double lambda=0, b=0, A=0;
+  double denom=0;
 
-    /* first, avoid omega0=1 because of instability */
-    if (omega0 > 0.999999)
-        omega0=0.999999;
+  /* first, avoid omega0=1 because of instability */
+  if (omega0 > 0.999999)
+    omega0=0.999999;
 
-    alpha1= (1.0-omega0)+0.75*(1.0-omega0*g);
-    alpha2=-(1.0-omega0)+0.75*(1.0-omega0*g);
+  alpha1= (1.0-omega0)+0.75*(1.0-omega0*g);
+  alpha2=-(1.0-omega0)+0.75*(1.0-omega0*g);
+  
+  lambda=sqrt(alpha1*alpha1-alpha2*alpha2);
+  
+  A=1.0/(alpha2/(alpha1-lambda)*exp(lambda*dtau)-alpha2/(alpha1+lambda)*exp(-lambda*dtau));
+  
+  a11=A*2.0*lambda/alpha2;
+  a12=A*(exp(lambda*dtau)-exp(-lambda*dtau));
+  
+  b=0.5-0.75*g*mu0;
+  alpha3=-omega0*b; 
+  alpha4=omega0*(1-b);
+  
+  denom = (1.0/mu0/mu0-lambda*lambda);
+  alpha5=((alpha1-1.0/mu0)*alpha3-alpha2*alpha4)/denom;
+  alpha6=(alpha2*alpha3-(alpha1+1.0/mu0)*alpha4)/denom;
+  
+  a33=exp(-dtau/mu0);
+  
+  a13=alpha5*(1.0-(a11)*(a33))-alpha6*(a12);
+  a23=-(a12)*alpha5*(a33)+alpha6*((a33)-(a11));
 
-    lambda=sqrt(alpha1*alpha1-alpha2*alpha2);
-
-    A=1.0/(alpha2/(alpha1-lambda)*exp(lambda*dtau)-alpha2/(alpha1+lambda)*exp(-lambda*dtau));
-
-    a11=A*2.0*lambda/alpha2;
-    a12=A*(exp(lambda*dtau)-exp(-lambda*dtau));
-
-    b=0.5-0.75*g*mu0;
-    alpha3=-omega0*b; 
-    alpha4=omega0*(1-b);
-
-    denom = (1.0/mu0/mu0-lambda*lambda);
-    alpha5=((alpha1-1.0/mu0)*alpha3-alpha2*alpha4)/denom;
-    alpha6=(alpha2*alpha3-(alpha1+1.0/mu0)*alpha4)/denom;
-
-    a33=exp(-dtau/mu0);
-
-    a13=alpha5*(1.0-(a11)*(a33))-alpha6*(a12);
-    a23=-(a12)*alpha5*(a33)+alpha6*((a33)-(a11));
-
-    *t    = a11;
-    *r    = a12;
-    *tdir = a33;
-    *rdir = a13 / mu0;
-    *sdir = a23 / mu0;
+  *t    = a11;
+  *r    = a12;
+  *tdir = a33;
+  *rdir = a13 / mu0;
+  *sdir = a23 / mu0;
 }
